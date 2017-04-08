ca5249350e525cf02deb60b7b9c9c826eb03d0df
talkami <talkaminker@yahoo.com>
2017-04-08 16:37:03 +0300

    added an initial 'main' file

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,12 @@
+#include "Player.h"
+#include "Board.h"
+#include "Game.h"
+#include "Boat.h"
+#include <iostream>
+
+int main(int argc, char* argv[]){
+	if (argc < 4) {
+		std::cout << 'incorrect number of arguments given!' << std::endl;
+		return -1;
+	}
+}
