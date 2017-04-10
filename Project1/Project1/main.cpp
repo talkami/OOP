c3a62fbd1964a5ced900ca90fda3ba8516f39270
talkami <talkaminker@yahoo.com>
2017-04-10 11:14:10 +0300

    changed 'main'

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]){
 	}
 	DIR * dir;
 	if ((dir = opendir(path.c_str())) == NULL) {
-		std::cout << "Wrong path: <" << this->m_basePath << ">" << std::endl;
+		std::cout << "Wrong path: " << path << std::endl;
 		return -1;
 	}
 	closedir(dir);
@@ -20,21 +20,21 @@ int main(int argc, char* argv[]){
 	bool ErrorOccured = false;
 	SeaBattleBoardLister seaBattleBoardLister = SeaBattleBoardLister(path);
 	if (seaBattleBoardLister.getFilesList().size() == 0) {
-		std::cout << "Missing board file (*.sboard) looking in path: <" + path + ">" << std::endl;
+		std::cout << "Missing board file (*.sboard) looking in path: " << path  << std::endl;
 		ErrorOccured = true;
 	}else{
 		std::string boardFile = seaBattleBoardLister.getFilesList()[0];
 	}
 	AttackAFileLister algoAattackFileLister = AttackAFileLister(path);
 	if (algoAattackFileLister.getFilesList().size() == 0) {
-		std::cout << "Missing attack file for player A (*.attack-a) looking in path: <" + path + ">" << std::endl;
+		std::cout << "Missing attack file for player A (*.attack-a) looking in path: " << path << std::endl;
 		ErrorOccured = true;
 	}else{
 		std::string boardFile = algoAattackFileLister.getFilesList()[0];
 	}
 	AttackBFileLister algoBattackFileLister = AttackBFileLister(path);
 	if (algoBattackFileLister.getFilesList().size() == 0) {
-		std::cout << "Missing attack file for player B (*.attack-b) looking in path: <" + path + ">" << std::endl;
+		std::cout << "Missing attack file for player B (*.attack-b) looking in path: " << path << std::endl;
 		ErrorOccured = true;
 	}else{
 		std::string boardFile = algoBattackFileLister.getFilesList()[0];
