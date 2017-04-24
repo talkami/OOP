faace165d57186e9bef823e1f271698c0ac12499
talkami <talkaminker@yahoo.com>
2017-04-24 15:27:44 +0300

    added files for smart player

---
+++ Project2/Project2/SmartPlayer.cpp
@@ -0,0 +1,81 @@
+
+#include "SmartPlayer.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+
+SmartPlayer::SmartPlayer() : gameScore(0), totalScore(0), wins(0), numOfBoats(0) {
+}
+
+SmartPlayer::~SmartPlayer() {
+	delete[] & player_board;
+}
+
+void SmartPlayer::setBoard(int player, const char** board, int numRows, int numCols) {
+	memcpy(this->player_board, board, sizeof(char) * numRows * numCols);
+	this->playerNum = player;
+}
+
+bool SmartPlayer::init(const std::string & path) {
+
+	//CREATE FUNCTION!!!!!
+
+	return true;
+}
+
+std::pair<int, int> SmartPlayer::attack() {
+	//CREATE FUNCTION!!!
+
+	return std::pair<int, int>(-1,-1);
+}
+
+bool SmartPlayer::hasFinishedAttacking() {
+
+	//FUNCTION MAY NEED CORRECTING
+	if (this->attackNumber >= this->maxMoves) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void SmartPlayer::notifyOnAttackResult(int player, int row, int col, AttackResult result) {}
+
+bool SmartPlayer::hasNoMoreBoats() {
+	if (numOfBoats == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void SmartPlayer::addBoat() {
+	this->numOfBoats += 1;
+}
+
+void SmartPlayer::removeBoat() {
+	if (this->numOfBoats == 0) {
+		std::cout << "Error: Player has no more boats to remove." << std::endl;
+	}
+	this->numOfBoats -= 1;
+}
+
+int SmartPlayer::getNumOfBoats() {
+	return this->numOfBoats;
+}
+
+int SmartPlayer::getGameScore() {
+	return this->gameScore;
+}
+
+void SmartPlayer::addWin() {
+	this->wins++;
+}
+
+void SmartPlayer::increaseScore(int amount) {
+	this->gameScore += amount;
+	this->totalScore += amount;
+}
---
+++ Project2/Project2/SmartPlayer.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "IBattleshipGameAlgo.h"
+#include <vector>
+#include <utility> 
+
+class SmartPlayer : public IBattleshipGameAlgo {
+	char player_board[10][10];
+	int gameScore;
+	int totalScore;
+	int wins;
+	std::vector<std::pair<int, int>> attackMoves;
+	int attackNumber;
+	size_t maxMoves;
+	int numOfBoats;
+	int playerNum;
+
+public:
+	SmartPlayer();
+	~SmartPlayer();
+	virtual void setBoard(int player, const char** board, int numRows, int numCols) override;
+	virtual bool init(const std::string& path) override;
+	virtual std::pair<int, int> attack() override;
+	virtual void notifyOnAttackResult(int player, int row, int col, AttackResult result) override;
+	void getMoves(const std::string& attackFile);
+	void processLine(const std::string& line);
+	static std::vector<std::string> split(const std::string &s, char delim);
+	bool hasFinishedAttacking();
+	bool hasNoMoreBoats();
+	void addBoat();
+	void removeBoat();
+	int getNumOfBoats();
+	int getGameScore();
+	void addWin();
+	void increaseScore(int amount);
+
+	SmartPlayer(const SmartPlayer&) {}
+	SmartPlayer& operator=(const SmartPlayer&) {}
+
+};
