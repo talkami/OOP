f0ead226f2f8db702255dd0318f078db5e79f090
nogur9 <nogur9@gmail.com>
2017-05-07 12:15:13 +0300

    7.5

--- Project2/Project2/Board.h
+++ Project2/Project2/Board.h
@@ -5,32 +5,31 @@
 #include <utility> // for std::pair
 
 class Board {
-	Point* matrix[10][10];
+	Point* matrix[rows][cols];
 	bool errorArray[9] = { false };
 	char** playerABoard;
 	char** playerBBoard;
 	int rows; // i / x
 	int cols; // j / y
 
-	void addBoatToBoard(Point* point, int i, int j, int size, int player, Player* owner, Player* rival);
-	void addToPlayerBoard(char currentChar, int row, int col, Player* A, Player* B);
+	void addBoatToBoard(Point* point, int i, int j, int size, int player, CommonPlayer* owner, CommonPlayer* rival);
+	void addToPlayerBoard(char currentChar, int row, int col, CommonPlayer* A, CommonPlayer* B);
 	void checkBoatValidity();
 	bool checkBoard();
-	bool checkNumOfPlayersBoats(Player* A, Player* B);
+	bool checkNumOfPlayersBoats(CommonPlayer* A, CommonPlayer* B);
 public:
 	bool updateBoardAfterAttack (Point * point, AttackResult result);
-	bool loadBoard(const std::string& boardFile, Player* A, Player* B, int rows, int cols);
-	bool createBoard (char** board, Player* A, Player* B, int rows, int cols);
+	bool loadBoard(const std::string& boardFile, CommonPlayer* A, CommonPlayer* B, int rows, int cols);
+	bool createBoard (char** board, CommonPlayer* A, CommonPlayer* B, int rows, int cols);
 	void setVars ( int rows, int cols); 
 	Board();
- 	bool playerLoadBoard (char** playerBoard, Player* player, int rows, int cols); //brb
+ 	bool playerLoadBoard (char** playerBoard, CommonPlayer* player, int rows, int cols); //brb
 	~Board();
 	AttackResult play_attack(std::pair <int, int> attack, int attacker, bool* selfHit);
 	char** getPlayerABoard();
 	char** getPlayerBBoard();
 	void setInvalidAttack(int row, int col);
 	bool isValidAttack(int row, int col);
-
 	Board(const Board&) = delete;
 	Board& operator=(const Board&) = delete;
 
