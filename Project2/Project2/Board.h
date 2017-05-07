4e531796555868e1beeb4f64dcc8de7f72cdb05b
nogur9 <nogur9@gmail.com>
2017-05-07 11:30:27 +0300

    7.57.57.57.57.5

--- Project2/Project2/Board.h
+++ Project2/Project2/Board.h
@@ -9,6 +9,8 @@ class Board {
 	bool errorArray[9] = { false };
 	char** playerABoard;
 	char** playerBBoard;
+	int rows; // i / x
+	int cols; // j / y
 
 	void addBoatToBoard(Point* point, int i, int j, int size, int player, Player* owner, Player* rival);
 	void addToPlayerBoard(char currentChar, int row, int col, Player* A, Player* B);
@@ -16,9 +18,12 @@ class Board {
 	bool checkBoard();
 	bool checkNumOfPlayersBoats(Player* A, Player* B);
 public:
-
-	bool loadBoard(const std::string& boardFile, Player* A, Player* B);
+	bool updateBoardAfterAttack (Point * point, AttackResult result);
+	bool loadBoard(const std::string& boardFile, Player* A, Player* B, int rows, int cols);
+	bool createBoard (char** board, Player* A, Player* B, int rows, int cols);
+	void setVars ( int rows, int cols); 
 	Board();
+ 	bool playerLoadBoard (char** playerBoard, Player* player, int rows, int cols); //brb
 	~Board();
 	AttackResult play_attack(std::pair <int, int> attack, int attacker, bool* selfHit);
 	char** getPlayerABoard();
