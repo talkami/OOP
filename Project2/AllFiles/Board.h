730a5fddfd7703304acb22d6fbbc9e036e021906
nogur9 <nogur9@gmail.com>
2017-05-15 16:28:23 +0300

    dll

--- Project2/AllFiles/Board.h
+++ Project2/AllFiles/Board.h
@@ -13,18 +13,25 @@ protected:
 	int numOfCols;
 	bool hasAdjacentBoats;
 
+	//new fields
+	int PlayerANumOfBoats;
+	int PlayerBNumOfBoats;
+
+
 	virtual void addBoatToBoard(Point* point, int size, int player, IBattleshipGameAlgo* owner, IBattleshipGameAlgo* rival);
 	virtual void checkAdjacentBoat(Boat* boat, Point* point, int size, int direction, int player, IBattleshipGameAlgo* owner, IBattleshipGameAlgo* rival);
 	void mergeBoats(Boat* boat1, Boat* boat2, int direction);
 
 
 public:
-	Board() : matrix(nullptr), hasAdjacentBoats(false) {}
+	Board() : matrix(nullptr), hasAdjacentBoats(false), PlayerANumOfBoats(0), PlayerBNumOfBoats(0){}
 	virtual ~Board();
 	virtual void setPoint(int row, int col);
 	virtual void setVars();
 
 	Board(const Board&) = delete;
 	Board& operator=(const Board&) = delete;
+
+
 
 };
