0afb04ee40cad85044572ca6da459d50f41a3496
talkami <talkaminker@yahoo.com>
2017-06-01 15:27:52 +0300

    moved stuff from TournamentManager::initGame to main

--- ex3/ex3/main.cpp
+++ ex3/ex3/main.cpp
@@ -2,9 +2,22 @@
 
 int main(int argc, char* argv[]) {
 	bool initializedSuccessfully, successfulPlay;
-	TournamentManager* game = new TournamentManager();
+	TournamentManager* game;
+	std::string path = ".";
+	int threads = 4;
+	DIR * dir;
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i - 1], "-threads") == 0) {
+			this->numOfThreads = atoi(argv[i]) > 0 ? atoi(argv[i]) : 0;
+		}
+		else if (i == 1) {
+			path = argv[i];
+		}
+	}
 
-	initializedSuccessfully = game->initGame(argc, argv);
+	game = new TournamentManager();
+
+	initializedSuccessfully = game->initGame(path, threads);
 	if (!initializedSuccessfully) {
 		return -1;
 	}
